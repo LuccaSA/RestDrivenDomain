36346cafd841ba7d3edc7eeda3181fd29afb4e80
Nicolas <nfaugout@lucca.fr>
2015-11-26 15:12:23 +0100

    FIX issue #1

--- RDD.Domain/Models/RestCollection.cs
+++ RDD.Domain/Models/RestCollection.cs
@@ -393,7 +393,7 @@ namespace RDD.Domain.Models
 			}
 
 			//Si c'était un PUT/DELETE, on en profite pour affiner la réponse
-			if (verb != HttpVerb.GET && count == 0 && Any(i => true))
+			if (verb != HttpVerb.GET && count == 0 && items.Count() == 0 && Any(i => true))
 			{
 				throw new UnauthorizedException(String.Format("Verb {0} unauthorized on entity type {1}", verb, typeof(TEntity).Name));
 			}
