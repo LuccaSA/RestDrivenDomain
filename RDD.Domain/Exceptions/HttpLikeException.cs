02ad87881531c2a5e177a217003bdf9a8e11dcfe
Nicolas <nfaugout@lucca.fr>
2016-02-23 19:19:46 +0100

    ENH HttpLikeException : ajout du logLevel
    Par défaut les erreurs 500 sont remontées en ERROR dans les logs, les autres erreurs sont en INFO (c'était le comportement de iLuccaSDK)

--- RDD.Domain/Exceptions/HttpLikeException.cs
+++ RDD.Domain/Exceptions/HttpLikeException.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RDD.Domain.Contexts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -28,19 +29,28 @@ namespace RDD.Domain.Exceptions
 			}
 		}
 
-		public HttpLikeException(HttpStatusCode status, string message, Exception innerException, params object[] args)
+		public HttpLikeException(HttpStatusCode status, LogLevel logLevel, string message, Exception innerException, params object[] args)
 			: base(String.Format(message, args), innerException)
 		{
 			this.Status = status;
+
+			var logger = Resolver.Current().Resolve<ILogService>();
+			logger.Log(logLevel, String.Format("Error {0}, {1]", status, message));
 		}
-		public HttpLikeException(HttpStatusCode status) : this(status, null, null, new HashSet<object>().ToArray()) { }
-		public HttpLikeException(HttpStatusCode status, string message) : this(status, message, null, new HashSet<object>().ToArray()) { }
-		public HttpLikeException(HttpStatusCode status, string message, params object[] args) : this(status, message, null, args) { }
-		public HttpLikeException(HttpStatusCode status, Exception innerException) : this(status, innerException.Message, innerException, new HashSet<object>().ToArray()) { }
+		public HttpLikeException(HttpStatusCode status) : this(status, StatusToLogLevel(status), null, null, new HashSet<object>().ToArray()) { }
+		public HttpLikeException(HttpStatusCode status, string message) : this(status, StatusToLogLevel(status), message, null, new HashSet<object>().ToArray()) { }
+		public HttpLikeException(HttpStatusCode status, LogLevel logLevel, string message) : this(status, logLevel, message, null, new HashSet<object>().ToArray()) { }
+		public HttpLikeException(HttpStatusCode status, string message, params object[] args) : this(status, StatusToLogLevel(status), message, null, args) { }
+		public HttpLikeException(HttpStatusCode status, Exception innerException) : this(status, StatusToLogLevel(status), innerException.Message, innerException, new HashSet<object>().ToArray()) { }
 
 		public static HttpLikeException Parse(Exception e)
 		{
 			return e as HttpLikeException ?? new HttpLikeException(HttpStatusCode.InternalServerError, e);
+		}
+
+		private static LogLevel StatusToLogLevel(HttpStatusCode status)
+		{
+			return (int)status >= 500 ? LogLevel.ERROR : LogLevel.INFO;
 		}
 	}
 }
