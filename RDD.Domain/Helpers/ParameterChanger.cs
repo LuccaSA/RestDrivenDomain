262b4885cb46fa7bf816c27af296c45d6d717f70
Nico <nfaugout@lucca.fr>
2016-05-26 06:50:30 +0200

    ADD BooleanExpression from Lucca FIX #15

---
+++ RDD.Domain/Helpers/ParameterChanger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RDD.Domain.Helpers
+{
+	public class ParameterChanger : ExpressionVisitor
+	{
+		ParameterExpression Parameter { get; set; }
+
+		public ParameterChanger(ParameterExpression parameter)
+		{
+			Parameter = parameter;
+		}
+
+		protected override Expression VisitParameter(ParameterExpression node)
+		{
+			return Parameter;
+		}
+	}
+}
