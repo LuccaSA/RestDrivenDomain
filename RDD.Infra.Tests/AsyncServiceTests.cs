c12ba72e24dc480b9327ef024306e7947e28ec49
Riron <orioncharlier@gmail.com>
2016-07-05 17:23:21 +0200

    ADD Test de l'asyncService
    Ces tests avaient été écrits dans WS BI pour vérifier que ContinueAsync était bien testable suite à la PR mergée précédemment (cf https://github.com/LuccaSA/RestDrivenDomain/commit/27bd4d5f795fc20d99d85b39a4833731429095c2)
    Ils ont plus leur place dans RDD.

---
+++ RDD.Infra.Tests/AsyncServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using RDD.Domain;
+using RDD.Domain.Contexts;
+using RDD.Infra.BootStrappers;
+using RDD.Infra.Services;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RDD.Infra.Tests
+{
+	[TestClass]
+	public class AsyncServiceTests
+	{
+		private readonly IAsyncService _asyncService;
+
+		private Mock<ICollection> _mock { get; set; }
+
+		public AsyncServiceTests()
+		{
+			TestsBootStrapper.ApplicationStart();
+			
+			_asyncService = new AsyncService();
+		}
+
+		[TestInitialize]
+		public void Init()
+		{
+			TestsBootStrapper.ApplicationBeginRequest();
+
+			_mock = new Mock<ICollection>();
+			_mock.Setup(m => m.GetEnumerator()).Verifiable();
+		}
+
+		private void CallVerifiableMockMethod()
+		{
+			_mock.Object.GetEnumerator();
+		}
+
+		[TestMethod]
+		public async Task AsyncService_ShouldBeTestable_WhenCallingContinueAsync()
+		{
+			await _asyncService.ContinueAsync(() => CallVerifiableMockMethod());
+
+			_mock.Verify(m => m.GetEnumerator(), Times.Once());
+		}
+
+		[TestMethod]
+		public void AsyncService_ShouldBeTestable_WhenCallingRunInParallel()
+		{
+			var list = new List<int> { 1, 2, 3 };
+
+			_asyncService.RunInParallel<int>(list, (number) => CallVerifiableMockMethod());
+
+			_mock.Verify(m => m.GetEnumerator(), Times.Exactly(3));
+		}
+	}
+}
