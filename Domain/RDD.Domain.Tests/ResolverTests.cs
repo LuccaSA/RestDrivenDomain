538299b577b1154cba54e11f0a327cf459d59f58
Nico <nfaugout@lucca.fr>
2017-02-02 07:43:05 +0100

    FIX Resolving dependencies with parameter constr
    Les enregistrements de dépendances avec paramètre sont indépendantes de celles sans paramètres
    Ainsi à la résolution, plus de cast incohérent
    Et on gère même le typage des arguments
    
    Resolves #27

---
+++ Domain/RDD.Domain.Tests/ResolverTests.cs
@@ -0,0 +1,39 @@
+﻿using RDD.Domain.Tests.Models;
+using RDD.Infra.Contexts;
+using RDD.Infra.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RDD.Domain.Tests
+{
+	public class ResolverTests
+	{
+		[Fact]
+		public void ResolverShouldNotResolveClassWithArgumentsIfNotRegistered()
+		{
+			var resolver = new DependencyInjectionResolver();
+
+			resolver.Register<IFakeInterface, string>((arg1) => new OneArgumentConstructorClass(arg1));
+
+			//Should not fail
+			var result = resolver.Resolve<IFakeInterface, string>("test");
+			Assert.NotNull(result);
+
+			//Should throw resolver exception (good number of argument, but wrong type)
+			Assert.Throws<ResolverException>(() =>
+			{
+				result = resolver.Resolve<IFakeInterface, int>(1);
+			});
+
+			//Should throw resolver exception (parameterless constr not registered)
+			Assert.Throws<ResolverException>(() =>
+			{
+				result = resolver.Resolve<IFakeInterface>();
+			});
+		}
+	}
+}
--- RDD.Core.Infra/Contexts/DependencyInjectionResolver.cs
+++ RDD.Core.Infra/Contexts/DependencyInjectionResolver.cs
@@ -1,4 +1,5 @@
 ﻿using RDD.Domain;
+using RDD.Infra.Exceptions;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -11,6 +12,7 @@ namespace RDD.Infra.Contexts
 	public class DependencyInjectionResolver : IDependencyInjectionResolver
 	{
 		private ConcurrentDictionary<Type, object> _mappings = new ConcurrentDictionary<Type, object>();
+		private ConcurrentDictionary<Tuple<Type, Type>, object> _mappingsWithOneArg = new ConcurrentDictionary<Tuple<Type, Type>, object>();
 
 		//TODO : mutualiser le code des différentes méthodes
 		public void Register<TInterface>(Func<TInterface> constructor) where TInterface : class
@@ -19,7 +21,7 @@ namespace RDD.Infra.Contexts
 		}
 		public void Register<TInterface, TArg1>(Func<TArg1, TInterface> constructor) where TInterface : class
 		{
-			_mappings[typeof(TInterface)] = constructor;
+			_mappingsWithOneArg[new Tuple<Type, Type>(typeof(TInterface), typeof(TArg1))] = constructor;
 		}
 
 		public TInterface Resolve<TInterface>() where TInterface : class
@@ -28,21 +30,21 @@ namespace RDD.Infra.Contexts
 
 			if (!_mappings.ContainsKey(type))
 			{
-				throw new Exception(String.Format("Type {0} not handled by dependency injection", type));
+				throw new ResolverException(String.Format("Type {0} not handled by dependency injection", type));
 			}
 
 			return ((Func<TInterface>)_mappings[type])();
 		}
 		public TInterface Resolve<TInterface, TArg1>(TArg1 arg1) where TInterface : class
 		{
-			var type = typeof(TInterface);
+			var tuple = new Tuple<Type, Type>(typeof(TInterface), typeof(TArg1));
 
-			if (!_mappings.ContainsKey(type))
+			if (!_mappingsWithOneArg.ContainsKey(tuple))
 			{
-				throw new Exception(String.Format("Type {0} not handled by dependency injection", type));
+				throw new ResolverException(String.Format("Type {0} with argument {1} not handled by dependency injection", tuple.Item1, tuple.Item2));
 			}
 
-			return ((Func<TArg1, TInterface>)_mappings[type])(arg1);
+			return ((Func<TArg1, TInterface>)_mappingsWithOneArg[tuple])(arg1);
 		}
 	}
 }
