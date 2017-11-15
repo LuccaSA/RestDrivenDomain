cdf6d677927a6dc0852d1100ac83178612da2e3f
jbbeuzelin <jbbeuzelin.dev@gmail.com>
2017-11-15 15:55:10 +0100

    UPDATE [WEB] > quick clean code

--- Web/RDD.Web/Serialization/PropertySerializer.cs
+++ Web/RDD.Web/Serialization/PropertySerializer.cs
@@ -36,79 +36,77 @@ namespace RDD.Web.Serialization
 
         public virtual object SerializeProperty(object entity, PropertySelector field)
         {
-            object value;
             PropertyInfo prop = field.GetCurrentProperty();
             var entityBase = entity as IEntityBase;
 
-            if (prop.Name == "Url" && entityBase != null)
+            var isUrlFieldOnEntityBase = prop.Name == "Url" && entityBase != null;
+            if (isUrlFieldOnEntityBase)
             {
-                value = _urlProvider.GetEntityUrl(entityBase) ;
+                return _urlProvider.GetEntityUrl(entityBase);
             }
-            else
+
+            var value = prop.GetValue(entity, null);
+
+            if (value != null)
             {
-                value = prop.GetValue(entity, null);
+                var propertyType = value.GetType();
 
-                if (value != null)
+                //Prop non valeur, on va sérialiser ses sous propriétés
+                if (!propertyType.IsValueType() && propertyType != typeof(object))
                 {
-                    var propertyType = value.GetType();
-
-                    //Prop non valeur, on va sérialiser ses sous propriétés
-                    if (!propertyType.IsValueType() && propertyType != typeof(object))
+                    if (propertyType.IsSubclassOfInterface(typeof(IDictionary)))
                     {
-                        if (propertyType.IsSubclassOfInterface(typeof(IDictionary)))
-                        {
-                            var dictionary = value as IDictionary;
+                        var dictionary = value as IDictionary;
 
-                            //Si le dictionnaire ne contient aucune entrée
+                        //Si le dictionnaire ne contient aucune entrée
+                        //On le sérialise tel quel
+                        if (dictionary != null && dictionary.Keys.Count > 0)
+                        {
+
+                            //Si c'st un dictionnaire de valeurs simples ou toutes nulles
                             //On le sérialise tel quel
-                            if (dictionary != null && dictionary.Keys.Count > 0)
+                            var enumerator = dictionary.Values.GetEnumerator();
+                            var isNullDictionary = true;
+                            while (enumerator.MoveNext())
                             {
-
-                                //Si c'st un dictionnaire de valeurs simples ou toutes nulles
-                                //On le sérialise tel quel
-                                var enumerator = dictionary.Values.GetEnumerator();
-                                var isNullDictionary = true;
-                                while (enumerator.MoveNext())
+                                if (enumerator.Current != null)
                                 {
-                                    if (enumerator.Current != null)
-                                    {
-                                        isNullDictionary = false;
-                                        break;
-                                    }
-                                }
-                                if (!isNullDictionary && !enumerator.Current.GetType().IsValueType())
-                                {
-                                    //Sinon on va descendre au niveau du type des valeurs pour continuer à les sérialiser
-                                    var result = new Dictionary<object, object>();
-
-                                    foreach (var key in dictionary.Keys)
-                                    {
-                                        result.Add(
-                                            key,
-                                            dictionary[key] != null ? _serializer.SerializeEntity(dictionary[key], PropertySelector.NewFromType(dictionary[key].GetType())) : null
-                                        );
-                                    }
-
-                                    value = result;
+                                    isNullDictionary = false;
+                                    break;
                                 }
                             }
-                        }
-                        else if (propertyType.IsEnumerableOrArray())
-                        {
-                            var list = ((IEnumerable)value).Cast<object>();
-                            var genericType = propertyType.GetEnumerableOrArrayElementType();
-
-                            //Si c'est une liste d'élément valeur, on sort d'ici, car on la sérialize telle quelle
-                            if (!genericType.IsValueType() && genericType != typeof(object))
+                            if (!isNullDictionary && !enumerator.Current.GetType().IsValueType())
                             {
+                                //Sinon on va descendre au niveau du type des valeurs pour continuer à les sérialiser
+                                var result = new Dictionary<object, object>();
 
-                                value = _serializer.SerializeEntities(list, field);
+                                foreach (var key in dictionary.Keys)
+                                {
+                                    result.Add(
+                                        key,
+                                        dictionary[key] != null ? _serializer.SerializeEntity(dictionary[key], PropertySelector.NewFromType(dictionary[key].GetType())) : null
+                                    );
+                                }
+
+                                value = result;
                             }
                         }
-                        else
+                    }
+                    else if (propertyType.IsEnumerableOrArray())
+                    {
+                        var list = ((IEnumerable)value).Cast<object>();
+                        var genericType = propertyType.GetEnumerableOrArrayElementType();
+
+                        //Si c'est une liste d'élément valeur, on sort d'ici, car on la sérialize telle quelle
+                        if (!genericType.IsValueType() && genericType != typeof(object))
                         {
-                            value = _serializer.SerializeEntity(value, field);
+
+                            value = _serializer.SerializeEntities(list, field);
                         }
+                    }
+                    else
+                    {
+                        value = _serializer.SerializeEntity(value, field);
                     }
                 }
             }
