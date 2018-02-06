fc23e1e58ffe7de6d96f495fe747cc6adf1c9ef1
Nicolas Faugout <nfaugout@lucca.fr>
2018-02-06 08:06:22 +0100

    ADD : TestFixture with principal
    This is a simple test that aims to demonstrate how to setup a principal inside a fixture/injection-ready test

---
+++ Infra/RDD.Infra.Tests/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using RDD.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RDD.Infra.Tests.Models
+{
+    public class User : EntityBase<User, int>
+    {
+        public override int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ Infra/RDD.Infra.Tests/RepositoryTestsFixture.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+using RDD.Domain;
+using RDD.Domain.Helpers;
+using RDD.Domain.Models;
+using RDD.Infra.Contexts;
+using RDD.Infra.Storage;
+using RDD.Infra.Tests.Models;
+using RDD.Infra.Tests.Repositories;
+using SimpleInjector;
+using SimpleInjector.Lifestyles;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RDD.Infra.Tests
+{
+    public class RepositoryTestsFixture
+    {
+        protected Container Container;
+
+        public RepositoryTestsFixture()
+        {
+            Container = new Container();
+            Container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
+            Container.Options.AllowOverridingRegistrations = true;
+
+            Container.Register<IStorageService, InMemoryStorageService>(Lifestyle.Scoped);
+
+            Container.Register<UsersRepository>(Lifestyle.Scoped);
+
+            Container.Register<IExecutionContext>(() =>
+            {
+                var executionContext = new HttpExecutionContext();
+                var principal = new Mock<IPrincipal>();
+                principal.Setup(p => p.HasOperation(It.IsAny<int>()))
+                    .Returns(true);
+                principal.Setup(p => p.HasAnyOperations(It.IsAny<HashSet<int>>()))
+                    .Returns(true);
+                principal.Setup(p => p.ApplyRights<User>(It.IsAny<IQueryable<User>>(), It.IsAny<HashSet<int>>()))
+                    .Returns<IQueryable<User>, HashSet<int>>((entities, operations) => entities);
+
+                executionContext.curPrincipal = principal.Object;
+
+                return executionContext;
+            }, Lifestyle.Scoped);
+            Container.Register<ICombinationsHolder>(() =>
+            {
+                var combinationHolder = new Mock<ICombinationsHolder>();
+                combinationHolder.Setup(c => c.Combinations)
+                    .Returns(new List<Combination>
+                    {
+                        new Combination { Operation = new Operation { Id = 1 }, Verb = HttpVerbs.Get, Subject = typeof(User) }
+                    });
+
+                return combinationHolder.Object;
+            }, Lifestyle.Scoped);
+        }
+
+        public Scope GetScope()
+        {
+            return AsyncScopedLifestyle.BeginScope(Container);
+        }
+    }
+}
