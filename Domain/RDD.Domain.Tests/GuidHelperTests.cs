c0f9739d98bf14f534970c30397f18eff6e16bbf
Nicolas Faugout <nfaugout@lucca.fr>
2018-04-12 09:03:43 +0200

    FIX : Guid in where query clause

---
+++ Domain/RDD.Domain.Tests/GuidHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using RDD.Domain.Helpers;
+using RDD.Domain.Models.Querying;
+using RDD.Domain.Tests.Models;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace RDD.Domain.Tests
+{
+    public class GuidHelperTests
+    {
+        private GuidHelper _helper;
+
+        public GuidHelperTests()
+        {
+            _helper = new GuidHelper();
+        }
+
+        [Theory]
+        [InlineData("aabbccdd-eeff", "Incomplete Guid with dashes")]
+        [InlineData("aabbccdd-eeff-1111-2222-333333333333", "Full Guid")]
+        [InlineData("aabbccddeeff", "Incomplete Guid without dash")]
+        public void InterpreteStringAsGuid_WHEN_WellFormedStringGuid(string input, string label)
+        {
+            try
+            {
+                _helper.Complete(input);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Fail for label: {label}", e);
+            }
+        }
+    }
+}
---
+++ Web/RDD.Web.Tests/FilterParserTests.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json.Serialization;
+using RDD.Domain.Helpers;
+using RDD.Web.Helpers;
+using RDD.Web.Tests.Models;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Xunit;
+
+namespace RDD.Web.Tests
+{
+    public class FilterParserTests
+    {
+        [Fact]
+        public void LikeOperationOnGuidShouldWork()
+        {
+            var httpContextAccessor = new HttpContextAccessor
+            {
+                HttpContext = new DefaultHttpContext()
+            };
+            httpContextAccessor.HttpContext.Request.QueryString = QueryString.Create("pictureId", "like,aabbccdd-eeff");
+            var helper = new ApiHelper<User, int>(httpContextAccessor ,null, null);
+            var query = helper.CreateQuery(HttpVerbs.Get);
+        }
+    }
+}
