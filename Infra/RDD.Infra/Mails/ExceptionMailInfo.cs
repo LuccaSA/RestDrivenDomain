5a038a716d032e6ebebf3a9465a0a4ccbe8628f4
romain V <rvergnory@lucca.fr>
2018-07-19 13:31:25 +0200

    REM : mails
    passer par Lucca.Core.Mail désormais

--- Infra/RDD.Infra/Mails/ExceptionMailInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RDD.Infra.Mails
-{
-    public class ExceptionMailInfo
-    {
-        public string Sender { get; set; }
-        public string Recipient { get; set; }
-        public string Subject { get; set; }
-    }
-}
