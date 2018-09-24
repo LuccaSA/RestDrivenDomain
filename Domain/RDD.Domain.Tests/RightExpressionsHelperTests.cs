267e6bdde74ce88a5f1eb7204581e8473a124f75
romain V <rvergnory@lucca.fr>
2018-09-24 15:16:39 +0200

    ADD : tests pour le rightExpressionshelper

---
+++ Domain/RDD.Domain.Tests/RightExpressionsHelperTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using RDD.Domain.Exceptions;
+using RDD.Domain.Models;
+using RDD.Domain.Rights;
+using RDD.Domain.Tests.Models;
+using System.Collections.Generic;
+using Xunit;
+
+namespace RDD.Domain.Tests
+{
+    public class RightExpressionsHelperTests
+    {
+        [Fact]
+        public void RightExpressionsHelper()
+        {
+            var holder = new Mock<ICombinationsHolder>();
+            holder.Setup(h => h.Combinations).Returns(new List<Combination>());
+
+            var anonymousHelper = new RightExpressionsHelper<User>(null, holder.Object);
+            Assert.Throws<ForbiddenException>(() => anonymousHelper.GetFilter(null));
+
+            var helper = new RightExpressionsHelper<User>(new Mock<IPrincipal>().Object, holder.Object);
+            Assert.Throws<UnreachableEntityException>(() => helper.GetFilter(new Domain.Models.Querying.Query<User>()));
+
+
+            holder.Setup(h => h.Combinations).Returns(new List<Combination> { new Combination { Verb = Helpers.HttpVerbs.Get, Subject = typeof(User), Operation = new Operation() } });
+            var filter = helper.GetFilter(new Domain.Models.Querying.Query<User> { Verb = Helpers.HttpVerbs.Get });
+
+            Assert.True(filter.Compile()(null));
+        }
+    }
+}
