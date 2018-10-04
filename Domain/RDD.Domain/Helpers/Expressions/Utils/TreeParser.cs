ca5954fac13d18370e5d56179ae94cc7ef4fd14e
romain V <rvergnory@lucca.fr>
2018-10-04 14:37:11 +0200

    MOD: assemblyinfo location

--- Domain/RDD.Domain/Helpers/Expressions/Utils/TreeParser.cs
+++ Domain/RDD.Domain/Helpers/Expressions/Utils/TreeParser.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("RDD.Domain.Tests")]
 namespace RDD.Domain.Helpers.Expressions.Utils
 {
     internal class TreeParser
