07256e46545d36e27656bec1ec876179beeb087b
Nicolas Faugout <nfaugout@lucca.fr>
2018-10-12 21:05:16 +0200

    MOD : EntityBase propose désormais une implémentation par défaut pour Id, Name.
    J'ai essayé de virer le set; de Name mais ça n'est pas possible. Si dans un projet on ne veut pas imposer le set; de Name il faut implémenter IEntityBase plutôt que dériver de EntityBase.

--- Web/RDD.Web.Tests/Models/Department.cs
+++ Web/RDD.Web.Tests/Models/Department.cs
@@ -5,8 +5,6 @@ namespace Rdd.Web.Tests.Models
 {
     public class Department : EntityBase<int>
     {
-        public override int Id { get; set; }
-        public override string Name { get; set; }
         public ICollection<User> Users { get; set; }
 
         public Department()
