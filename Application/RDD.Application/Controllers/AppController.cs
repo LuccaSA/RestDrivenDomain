f38ec085a940f5d2aca2f44465b253dc84942749
Raphaël DUCOM <raphael.ducom@gmail.com>
2018-10-15 12:54:33 +0200

    On retire la possibilité d'appel à IUnitOfWork.SaveChangesAsync sans déclencher les hooks

--- Application/RDD.Application/Controllers/AppController.cs
+++ Application/RDD.Application/Controllers/AppController.cs
@@ -23,74 +23,64 @@ namespace Rdd.Application.Controllers
         where TEntity : class, IEntityBase<TKey>
         where TKey : IEquatable<TKey>
     {
-        protected IUnitOfWork UnitOfWork { get; }
+        private readonly IUnitOfWork _unitOfWork;
 
         public AppController(IUnitOfWork unitOfWork, TCollection collection)
             : base(collection)
         {
-            UnitOfWork = unitOfWork;
+            _unitOfWork = unitOfWork;
         }
 
         public virtual async Task<TEntity> CreateAsync(ICandidate<TEntity, TKey> candidate, Query<TEntity> query)
         {
             var entity = await Collection.CreateAsync(candidate, query);
-
-            await OnBeforeSaveEntitiesAsync(entity.Yield());
-            await UnitOfWork.SaveChangesAsync();
-            await OnAfterSaveEntitiesAsync(entity.Yield());
-
+            await SaveChangesAsync(entity.Yield());
             return entity;
         }
 
         public virtual async Task<IEnumerable<TEntity>> CreateAsync(IEnumerable<ICandidate<TEntity, TKey>> candidates, Query<TEntity> query)
         {
             var entities = await Collection.CreateAsync(candidates, query);
-
-            await OnBeforeSaveEntitiesAsync(entities);
-            await UnitOfWork.SaveChangesAsync();
-            await OnAfterSaveEntitiesAsync(entities);
-
+            await SaveChangesAsync(entities);
             return entities;
         }
 
         public virtual async Task<TEntity> UpdateByIdAsync(TKey id, ICandidate<TEntity, TKey> candidate, Query<TEntity> query)
         {
             var entity = await Collection.UpdateByIdAsync(id, candidate, query);
-
-            await OnBeforeSaveEntitiesAsync(entity.Yield());
-            await UnitOfWork.SaveChangesAsync();
-            await OnAfterSaveEntitiesAsync(entity.Yield());
-
+            await SaveChangesAsync(entity.Yield());
             return entity;
         }
 
         public virtual async Task<IEnumerable<TEntity>> UpdateByIdsAsync(IDictionary<TKey, ICandidate<TEntity, TKey>> candidatesByIds, Query<TEntity> query)
         {
             var entities = await Collection.UpdateByIdsAsync(candidatesByIds, query);
-
-            await OnBeforeSaveEntitiesAsync(entities);
-            await UnitOfWork.SaveChangesAsync();
-            await OnAfterSaveEntitiesAsync(entities);
-
+            await SaveChangesAsync(entities);
             return entities;
         }
 
         public async Task DeleteByIdAsync(TKey id)
         {
             await Collection.DeleteByIdAsync(id);
-
-            await OnBeforeSaveEntitiesAsync(Enumerable.Empty<TEntity>());
-            await UnitOfWork.SaveChangesAsync();
-            await OnAfterSaveEntitiesAsync(Enumerable.Empty<TEntity>());
+            await SaveChangesAsync(Enumerable.Empty<TEntity>());
         }
 
         public async Task DeleteByIdsAsync(IEnumerable<TKey> ids)
         {
             await Collection.DeleteByIdsAsync(ids);
+            await SaveChangesAsync(Enumerable.Empty<TEntity>());
+        }
 
-            await OnBeforeSaveEntitiesAsync(Enumerable.Empty<TEntity>());
-            await UnitOfWork.SaveChangesAsync();
-            await OnAfterSaveEntitiesAsync(Enumerable.Empty<TEntity>());
+        /// <summary>
+        /// Calls UnitOfWork.SaveChangesAsync() and pass modified items to OnBefore / OnAfter methods
+        /// </summary>
+        /// <param name="entities"></param>
+        /// <returns></returns>
+        protected virtual async Task SaveChangesAsync(IEnumerable<TEntity> entities)
+        {
+            await OnBeforeSaveEntitiesAsync(entities);
+            await _unitOfWork.SaveChangesAsync();
+            await OnAfterSaveEntitiesAsync(entities);
         }
 
         /// <summary>
