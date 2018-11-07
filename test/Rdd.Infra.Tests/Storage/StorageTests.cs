98ea9606993bbc3be244b7d59e8d5f6c0026818b
romain V <rvergnory@lucca.fr>
2018-11-07 15:34:33 +0100

    ADD : couverture de tests projet Infra

---
+++ test/Rdd.Infra.Tests/Storage/StorageTests.cs
@@ -0,0 +1,64 @@
+﻿using Rdd.Domain.Models.Querying;
+using Rdd.Domain.Rights;
+using Rdd.Domain.Tests.Models;
+using Rdd.Infra.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rdd.Infra.Tests.Storage
+{
+    public class StorageTests
+    {
+        private readonly EFStorageService _efStorage = new EFStorageService(new DataContext());
+        private readonly InMemoryStorageService _inMemory = new InMemoryStorageService();
+
+        [Fact]
+        public void CstrThrowsWhenNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new EFStorageService(null));
+        }
+
+        [Fact]
+        public async Task EnumerateThrowsWhenNull()
+        {
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await _efStorage.EnumerateEntitiesAsync<User>(null));
+        }
+
+        [Fact]
+        public async Task EnumerateDoesNotFailWhenQueryableIsNotFromEF()
+        {
+            var queryable = new List<User>().AsQueryable();
+            var result = await _efStorage.EnumerateEntitiesAsync(queryable);
+        }
+
+        [Fact]
+        public void InMemoryDeleteRange()
+        {
+            var user = new User();
+            _inMemory.Add(user);
+            Assert.Single(_inMemory.Set<User>());
+            _inMemory.Remove(user);
+            Assert.Empty(_inMemory.Set<User>());
+
+            var users = new List<User> { new User(), new User() };
+            _inMemory.AddRange(users);
+            Assert.Equal(2, _inMemory.Set<User>().ToList().Count);
+            _inMemory.RemoveRange(users);
+            Assert.Empty(_inMemory.Set<User>());
+
+        }
+
+        [Fact]
+        public async Task RepoOnNullFilterDoesNotFail()
+        {
+            var repo = new Repository<User>(new InMemoryStorageService(), new OpenRightExpressionsHelper<User>());
+            repo.Add(new User());
+
+            var count = await repo.CountAsync(new Query<User> { Filter = null });
+            Assert.Equal(1, count);
+        }
+    }
+}
