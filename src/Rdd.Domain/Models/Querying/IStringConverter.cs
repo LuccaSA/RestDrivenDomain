92c1016e80aabf4e7d157db555a043e939735b5d
Raphaël DUCOM <raphael.ducom@gmail.com>
2018-11-09 08:42:43 +0100

    memoryleak fix, first pass

--- src/Rdd.Domain/Models/Querying/IStringConverter.cs
+++ src/Rdd.Domain/Models/Querying/IStringConverter.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Rdd.Domain.Models.Querying
 {
@@ -11,5 +14,29 @@ namespace Rdd.Domain.Models.Querying
 
         IList ConvertValues(Type type, IEnumerable<string> values);
         object ConvertTo(Type type, string input);
+    }
+
+    public static class ExpressionExtension
+    {
+        public static Expression ExtractExpression<T>(this T value)
+        {
+            if (typeof(T) == typeof(object))
+            {
+                throw new NotSupportedException("Only use typed values");
+            }
+            return ((Expression<Func<T>>)(() => value)).Body;
+        }
+
+        public static Expression ExtractTypedExpression(this object value, Type type)
+        {
+            var method = typeof(ExpressionExtension)
+                .GetMethod(nameof(ExtractExpression),BindingFlags.Public | BindingFlags.Static);
+            if (method == null)
+            {
+                throw new NotSupportedException();
+            }
+            var generic = method.MakeGenericMethod(type);
+            return (Expression)generic.Invoke(null, new object[] { value });
+        }
     }
 }
